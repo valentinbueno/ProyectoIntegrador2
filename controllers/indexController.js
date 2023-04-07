4fc20c82415abea39cc5bd57183532ab696d4aeb
Felipejordan1 <fjordan@udesa.edu.ar>
2023-04-07 17:51:31 -0300

    cuarto avance

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,12 @@
+const data = require ('../data/data')
 const controllerIndex = {
     index: function(req, res) {
-        res.render('index')
+        res.render('index', {
+          products:data.products,
+          user:false,
+        })
       }
 }
 
 module.exports = controllerIndex;
+
---
+++ data/data.js
@@ -0,0 +1,147 @@
+const data = {
+    products: [
+        {
+            imagen:'/images/products/img-macbook-pro-2019.jpg',
+            nombre: 'MAC AIR 1',
+            descripción:'hjaskshdkhdsjdhskjdshjkdshshs',
+            fechaDeCarga:'05/07/2022',
+            comments: [
+                {
+                    usuario:'Jose',
+                    comentario: 'muy buen producto'
+                },
+            ],
+        },
+
+        {
+            imagen:'/images/products/img-macbook-pro-2019.jpg',
+            nombre: 'MAC AIR 2',
+            descripción:'hjaskshdkhdsjdhskjdshjkdshshs',
+            fechaDeCarga:'05/07/2022',
+            comments: [
+                {
+                    usuario:'Jose',
+                    comentario: 'muy buen producto'
+                },
+            ],
+        },
+
+        {
+            imagen:'/images/products/img-macbook-pro-2019.jpg',
+            nombre: 'MAC AIR 3',
+            descripción:'hjaskshdkhdsjdhskjdshjkdshshs',
+            fechaDeCarga:'05/07/2022',
+            comments: [
+                {
+                    usuario:'Jose',
+                    comentario: 'muy buen producto'
+                },
+            ],
+        },
+
+        {
+            imagen:'/images/products/img-macbook-pro-2019.jpg',
+            nombre: 'MAC AIR 4',
+            descripción:'hjaskshdkhdsjdhskjdshjkdshshs',
+            fechaDeCarga:'05/07/2022',
+            comments: [
+                {
+                    usuario:'Jose',
+                    comentario: 'muy buen producto'
+                },
+            ],
+        },
+
+        {
+            imagen:'/images/products/img-macbook-pro-2019.jpg',
+            nombre: 'MAC AIR 5',
+            descripción:'hjaskshdkhdsjdhskjdshjkdshshs',
+            fechaDeCarga:'05/07/2022',
+            comments: [
+                {
+                    usuario:'Jose',
+                    comentario: 'muy buen producto'
+                },
+            ],
+        },
+
+        {
+            imagen:'/images/products/img-macbook-pro-2019.jpg',
+            nombre: 'MAC AIR 6',
+            descripción:'hjaskshdkhdsjdhskjdshjkdshshs',
+            fechaDeCarga:'05/07/2022',
+            comments: [
+                {
+                    usuario:'Jose',
+                    comentario: 'muy buen producto'
+                },
+            ],
+        },
+
+        {
+            imagen:'/images/products/img-macbook-pro-2019.jpg',
+            nombre: 'MAC AIR 7',
+            descripción:'hjaskshdkhdsjdhskjdshjkdshshs',
+            fechaDeCarga:'05/07/2022',
+            comments: [
+                {
+                    usuario:'Jose',
+                    comentario: 'muy buen producto'
+                },
+            ],
+        },
+
+        {
+            imagen:'/images/products/img-macbook-pro-2019.jpg',
+            nombre: 'MAC AIR 8',
+            descripción:'hjaskshdkhdsjdhskjdshjkdshshs',
+            fechaDeCarga:'05/07/2022',
+            comments: [
+                {
+                    usuario:'Jose',
+                    comentario: 'muy buen producto'
+                },
+            ],
+        },
+
+        {
+            imagen:'/images/products/img-macbook-pro-2019.jpg',
+            nombre: 'MAC AIR 9',
+            descripción:'hjaskshdkhdsjdhskjdshjkdshshs',
+            fechaDeCarga:'05/07/2022',
+            comments: [
+                {
+                    usuario:'Jose',
+                    comentario: 'muy buen producto'
+                },
+            ],
+        },
+
+        {
+            imagen:'/images/products/img-macbook-pro-2019.jpg',
+            nombre: 'MAC AIR 10',
+            descripción:'hjaskshdkhdsjdhskjdshjkdshshs',
+            fechaDeCarga:'05/07/2022',
+            comments: [
+                {
+                    usuario:'Jose',
+                    comentario: 'muy buen producto'
+                },
+            ],
+        },
+
+
+
+
+        
+
+    ],
+
+    user: {
+
+
+    },
+   
+}
+
+module.exports= data
