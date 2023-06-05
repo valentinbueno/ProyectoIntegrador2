8b2d01d859747515391dd8f4575f9128214840a7
SantiagoVillares <svillares@udesa.edu.ar>
2023-06-05 11:16:06 -0300

    index

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,11 +1,22 @@
 const data = require ('../data/data')
+
+const db = require('../database/models')
 const controllerIndex = {
     index: function(req, res) {
+
+
+      db.Producto.findAll()
+      .then(function(result) {
         res.render('index', {
-          products:data.products,
+          products: result,
           user:data.user,
           userLogueado: true,
         })
+      }).catch(function(err) {
+        console.log(err);
+      })
+
+        
       }
 }
 
